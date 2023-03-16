08681a13ce7b1a9aba0a71e21223d8899d73d031
Epouvaantail <coutouzis.kilian@gmail.com>
2023-03-16 14:05:09 +0100

    up

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -1,18 +1,55 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		image:"slide1.jpg",
+		tagLine:"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
+
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		image:"slide2.jpg",
+		tagLine:"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
+
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		image:"slide3.jpg",
+		tagLine:"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
+
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		image:"slide4.png",
+		tagLine:"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+const images = slides.map(slide => slide.image);
+const tagLine = slides.map(slide => slide.tagLine);
+let currentSlide = 0;
+let maxSlide = slides.length -1;
+
+function SlideChange(sens) {
+	currentSlide = currentSlide + sens
+	if (currentSlide > maxSlide)
+		currentSlide = 0;
+	if (currentSlide < 0)
+		currentSlide = maxSlide;
+
+	document.getElementById("slides").src = "./assets/images/slideshow/" + images[currentSlide];
+	document.getElementById("tagLine").innerHTML =" " + tagLine[currentSlide]
+	document.getElementById("dot"+[currentSlide]).className += " dot_selected"
+	document.getElementById("dot"+[currentSlide-sens]).className -= " dot_selected"
+	document.getElementById("dot"+[currentSlide-sens]).className += " dot"
+	document.getElementById("dot"+[currentSlide+sens]).className -= " dot_selected"
+	document.getElementById("dot"+[currentSlide+sens]).className += " dot"
+}
+
+// function LeftArrow() {
+// 	alert('left')
+// }
+
+// function RightArrow() {
+// 	alert('right')
+// }
+
+// arrow_left.addEventListener('click',LeftArrow)
+// arrow_right.addEventListener('click',RightArrow)
+
+// setInterval("SlideChange(1)", 4000);
