c87bd4ef68211e843c14a388b1612e85ac71d5f7
Epouvaantail <coutouzis.kilian@gmail.com>
2023-03-21 14:57:49 +0100

    up

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -20,36 +20,94 @@ const slides = [
 	}
 ]
 
-const images = slides.map(slide => slide.image);
-const tagLine = slides.map(slide => slide.tagLine);
+// const dots = document.querySelector(".dots");
+// const images = slides.map(slide => slide.image);
+// const tagLine = slides.map(slide => slide.tagLine);
+// let currentSlide = 0;
+// let maxSlide = slides.length -1;
+// const arrowLeft = document.querySelector(".arrow_left");
+// const arrowRight = document.querySelector(".arrow_right");
+
+
+// function SlideChange(sens) {
+// 	currentSlide = currentSlide + sens
+// 	if (currentSlide > maxSlide)
+// 		currentSlide = 0;
+// 	if (currentSlide < 0)
+// 		currentSlide = maxSlide;
+// 	document.getElementById("slides").src = "./assets/images/slideshow/" + images[currentSlide];
+// 	document.getElementById("tagLine").innerHTML =" " + tagLine[currentSlide];
+	// if (document.getElementById("dot"+[currentSlide])){
+	// 	document.getElementById("dot"+[currentSlide]).className += " dot_selected";
+	// 	document.getElementById("dot"+[currentSlide-sens]).className = "dot";
+	// }
+
+// }
+
+// for (let i = 0; i < slides.length; i++) {
+// 	const newElt =
+// 	document.createElement("div");
+// 	let elt =
+// 	document.getElementById("dots");
+// 	elt.appendChild(newElt);
+// 	newElt.classList.add("dot");
+// 	newElt.id += "dot"+[i];
+// }
+
+// document.getElementById("dot0").className += " dot_selected";
+
+
+const arrowLeft = document.querySelector(".arrow_left");
+const arrowRight = document.querySelector(".arrow_right");
+const image = document.querySelector(".banner-img");
+const slidesLength = slides.length;
+const maxSlide = slides.length - 1;
+const tagLine = document.querySelector(".tagLine");
 let currentSlide = 0;
-let maxSlide = slides.length -1;
+const dots = document.querySelector(".dots");
 
-function SlideChange(sens) {
-	currentSlide = currentSlide + sens
-	if (currentSlide > maxSlide)
-		currentSlide = 0;
-	if (currentSlide < 0)
-		currentSlide = maxSlide;
-	document.getElementById("slides").src = "./assets/images/slideshow/" + images[currentSlide];
-	document.getElementById("tagLine").innerHTML =" " + tagLine[currentSlide]
-	document.getElementById("dot"+[currentSlide]).className += " dot_selected"
-	document.getElementById("dot"+[currentSlide-sens]).className -= " dot_selected"
-	document.getElementById("dot"+[currentSlide-sens]).className += " dot"
-	document.getElementById("dot"+[currentSlide+sens]).className -= " dot_selected"
-	document.getElementById("dot"+[currentSlide+sens]).className += " dot"
 
+
+for (let i = 0; i < slidesLength; i++) {
+  const dot = document.createElement("div");
+  dot.className ="dot";
+  dots.appendChild(dot);
 }
 
-// function LeftArrow() {
-// 	alert('left')
-// }
+const dot = document.querySelectorAll(".dot");
+dot[0].classList.add("dot_selected");
 
-// function RightArrow() {
-// 	alert('right')
-// }
+arrowLeft.addEventListener("click", () => {
+  if (currentSlide === 0) {
+    dot[currentSlide].classList.remove("dot_selected");
+    image.src = "./assets/images/slideshow/" + slides[maxSlide].image;
+    tagLine.innerHTML = slides[maxSlide].tagLine;
+    dot[maxSlide].classList.add("dot_selected");
+    currentSlide = maxSlide
+  }
+  else {
+    dot[currentSlide].classList.remove("dot_selected");
+    image.src = "./assets/images/slideshow/" + slides[currentSlide - 1].image;
+    tagLine.innerHTML = slides[currentSlide- 1].tagLine;
+    dot[currentSlide-1].classList.add("dot_selected");
+    currentSlide = currentSlide - 1
+  }
+})
 
-// arrow_left.addEventListener('click',LeftArrow)
-// arrow_right.addEventListener('click',RightArrow)
-
-// setInterval("SlideChange(1)", 4000);
+arrowRight.addEventListener("click", () => {
+  if (currentSlide === maxSlide) {
+    dot[currentSlide].classList.remove("dot_selected");
+    image.src = "./assets/images/slideshow/" + slides[0].image;
+    tagLine.innerHTML = slides[0].tagLine;
+    dot[0].classList.add("dot_selected");
+    currentSlide = 0;
+  
+  }
+  else {
+    dot[currentSlide].classList.remove("dot_selected");
+    image.src = "./assets/images/slideshow/" + slides[currentSlide + 1].image;
+    tagLine.innerHTML = slides[currentSlide + 1].tagLine;
+    dot[currentSlide +1].classList.add("dot_selected");
+    currentSlide = currentSlide + 1;
+  }
+})
